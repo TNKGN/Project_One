4595327a1bde070066e6c6bfb045cd3b310e1335
TNKGN <toniakogan2013@gmail.com>
2023-11-28 11:16:49 +0200

    Homework

---
+++ Homework/Forms/skript.js
@@ -0,0 +1,17 @@
+// const subButton = document.querySelector(#'subButton');
+
+// subButton.onclick = function () {
+//     console.log('Click');
+// }
+
+const form = document.querySelector('form');
+
+
+
+const user = {
+    name: formData.get('name'),
+    age: form.Data.get('age'),
+    email: formData.get('email'),
+    phone: formData.get('phone')
+}
+console.log(JSON.stringify(user))
