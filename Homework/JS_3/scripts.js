8f918bbd708485efc6aaafc88d69bca335b33d69
TNKGN <toniakogan2013@gmail.com>
2023-12-29 00:06:45 +0200

    Homework

---
+++ Homework/JS_3/scripts.js
@@ -0,0 +1,102 @@
+//Task 1
+
+// const a = document.querySelector('.width');
+// const b = document.querySelector('.lenght');
+// const res = document.querySelector('.result')
+// const button = document.querySelector('.button');
+
+// button.addEventListener('click', area);
+
+// function area() {
+//     const area = (Number(a.value) + Number(b.value)) * 2;
+//     res.textContent = 'Результат: ' + area; 
+// }
+
+//Task 2
+
+// const a = document.querySelector('.width');
+// const b = document.querySelector('.lenght');
+// const res = document.querySelector('.area')
+// const btn = document.querySelector('.btn');
+
+// btn.addEventListener('click', area);
+
+// function area() {
+//     const area = Number(a.value) * Number(b.value);
+//     res.textContent = 'Результат: ' + area; 
+// }
+
+//Task 3
+
+// const a = document.querySelector('.side-1');
+// const b = document.querySelector('.side-2');
+// const c = document.querySelector('.side-3')
+// const res = document.querySelector('.result')
+// const btn = document.querySelector('.btn');
+
+// btn.addEventListener('click', perimeter);
+
+// function perimeter() {
+//     const perimeter = Number(a.value) + Number(b.value) + Number(c.value);
+//     res.textContent = 'Результат: ' + perimeter;
+// }
+
+//Task 4
+
+// const a = document.querySelector('.base');
+// const b = document.querySelector('.height');
+// const res = document.querySelector('.result')
+// const btn = document.querySelector('.btn');
+
+// btn.addEventListener('click', area);
+
+// function area() {
+//     const area = (Number(a.value) * Number(b.value)) / 2; 
+//     res.textContent = "Результат: " + area;
+// }
+
+//Task 5
+
+// const a = document.querySelector('.lenght');
+// const res = document.querySelector('.result');
+// const btn = document.querySelector('.btn');
+
+// btn.addEventListener('click', perimeter);
+
+// function perimeter() {
+//     const perimeter = Number(a.value) * 4;
+//     res.textContent = 'Результат: ' + perimeter; 
+// }
+
+//Task 6
+
+// const a = document.querySelector('.lenght');
+// const res = document.querySelector('.result');
+// const btn = document.querySelector('.btn');
+
+// btn.addEventListener('click', perimeter);
+
+// function perimeter() {
+//     const perimeter = Number(a.value) * Number(a.value);
+//     res.textContent = 'Результат: ' + perimeter; 
+// }
+
+//Task 7
+
+// const a = document.querySelector('.number');
+// const res = document.querySelector('.result');
+// const btn = document.querySelector('.btn');
+
+// btn.addEventListener('click', degree);
+
+// function degree() {
+//     const deg = Number(a.value) ** 3;
+//     res.textContent = 'Результат: ' + deg;
+// }
+
+
+
+
+
+
+
